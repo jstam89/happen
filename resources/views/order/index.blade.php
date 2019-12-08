c08fd5b94406f5385b2fa3ff96f3956a5981a162
jstam89 <jer.stam89@gmail.com>
2019-12-08 21:10:20 +0100

    added some js to cart

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -3,37 +3,90 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-7">
+
             @foreach($menus as $menu)
-                <div class="card">
+                <div class="card menu-item">
                     <div class="card-header ">
                         <div class="row">
                             <div class="col-sm-6 text-left">
                                 <h5 class="card-category">{{$menu->takeout_date}}</h5>
-                                <h2 class="card-title">{{$menu->title}}</h2>
+                                <h2 class="card-title menu-title">{{$menu->title}}</h2>
                             </div>
                         </div>
                     </div>
+
                     <div class="card-body">
                         <p>{{$menu->info}}</p>
                     </div>
-                    <div class="card-footer">
-                        <div class="dropdown-primary text-right">
-                            <select name="menu-select" class="form-control" style="background-color: #1e1e28">
-                                @for ($i = 0; $i <= 10; $i++)
-                                    <option name="order-quantity" value="<?php echo $i ?>"><?php echo $i ?></option>
-                                @endfor
-                            </select>
-                        </div>
 
+                    <div class="card-footer">
+                        <button type="submit"
+                                class="add-to-cart btn btn-icon pull-right ">
+                            <i class="fa fa-shopping-cart"></i>
+                        </button>
                     </div>
                 </div>
             @endforeach
+
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-5">
             @include('partials.cart')
         </div>
-
     </div>
+
+    <script async>
+
+        if (document.readyState === 'loading') {
+            document.addEventListener('DOMContentLoaded', ready)
+        } else {
+            ready();
+        }
+
+        function ready() {
+            const removeCartItemButton = document.getElementsByClassName('cart-delete');
+            for (let i = 0; i < removeCartItemButton.length; i++) {
+                const button = removeCartItemButton[i];
+                button.addEventListener('click', removeCartItem);
+            }
+
+            const addToCartButton = document.getElementsByClassName('add-to-cart');
+            for (let i = 0; i < addToCartButton.length; i++) {
+                const button = addToCartButton[i];
+                button.addEventListener('click', addToCartClicked);
+            }
+        }
+
+        function removeCartItem(event) {
+            const buttonClicked = event.target;
+            buttonClicked.parentElement.parentElement.parentElement.remove();
+        }
+
+        function addToCartClicked(event) {
+            const button = event.target;
+            const menuItem = button.parentElement.parentElement.parentElement;
+            const title = menuItem.getElementsByClassName('menu-title')[0].innerHTML;
+            addItemToCart(title)
+        }
+
+        function addItemToCart(title) {
+            var cartRow = document.createElement('td');
+            cartRow.innerText = title;
+            //TODO: add item to cart function
+
+        }
+
+
+        // function addToCart(event) {
+        //     var button = event.target;
+        //     const menuItem = button.parentElement.parentElement;
+        //     const title = menuItem.getElementsByClassName('menu-title');
+        //     console.log(title);
+        // }
+        //
+        // addToCart();
+    </script>
+
+
 @endsection
--- resources/views/partials/cart.blade.php
+++ resources/views/partials/cart.blade.php
@@ -1,16 +1,52 @@
 <div class="card">
     <div class="card-header">
         <h3 class="card-title">Plaats je bestelling</h3>
-
     </div>
+
     <div class="card-body">
+        <table class="table table-borderless">
+            <thead>
+            <th>Menu</th>
+            <th>Aantal</th>
+
+            </thead>
+
+            <tbody>
+            <tr class="cart-row">
+                <td class="menu-title">Pasta met Zalm</td>
+                <td><select name="quantity">
+                        <?php for ($i = 1; $i <= 10; $i++) : ?>
+                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                        <?php endfor; ?>
+                    </select></td>
+                <td>
+                    <a id="cart-item-delete" class="btn btn-sm btn-icon cart-delete" href="#" role="button">
+                        <i class="fas fa-times"></i></a>
+                </td>
+            </tr>
+            <tr>
+                <td>doreme</td>
+                <td><select name="quantity">
+                        <?php for ($i = 1; $i <= 10; $i++) : ?>
+                        <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+                        <?php endfor; ?>
+                    </select></td>
+                <td>
+                    <a id="cart-item-delete" class="btn btn-sm btn-icon cart-delete" href="#" role="button">
+                        <i class="fas fa-times"></i></a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="card-footer">
         <div>
-            <div class="result"></div>
-            <button type="submit" class="btn btn-block btn-primary">{{ __('Bestellens') }}</button>
+            <button type="submit" id="cart-submit" class="btn btn-primary">{{ __('Bestellen') }}</button>
         </div>
     </div>
-</div>
 
+</div>
 
 
 
