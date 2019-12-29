0d936a61915445547c36b5ce39d6259d365c535a
ardin0546 <ardin468@gmail.com>
2019-12-29 12:56:52 +0100

    Added menus seeder

---
+++ database/factories/MenusFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Menu::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'info' =>  $faker->paragraph,
+        'menu_image' => 'https://www.telegraaf.nl/images/840x473/filters:format(jpeg):quality(50)/cdn-kiosk-api.telegraaf.nl/1270efe0-64d6-11e8-878c-4d7aa66accf8.jpg',
+        'takeout_date' => \Carbon\Carbon::now()
+    ];
+});
---
+++ database/seeds/MenusTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MenusTableSeeder extends Seeder
+{
+    private $numberOfMenus = 3;
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Menu::class, $this->numberOfMenus)->create();
+    }
+}
