88c3d93d91a648eed9c9a7bcaae19b3e895dbd8a
jstam89 <jer.stam89@gmail.com>
2020-03-10 11:17:24 +0100

    minor fix

--- database/factories/MenusFactory.php
+++ database/factories/MenusFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Menu::class, function (Faker $faker) {
     return [
         'title'        => $faker->sentence,
-        'info'         => $faker->paragraph,
+        // 'info'         => $faker->paragraph,
         'menu_image'   => 'https://www.telegraaf.nl/images/840x473/filters:format(jpeg):quality(50)/cdn-kiosk-api.telegraaf.nl/1270efe0-64d6-11e8-878c-4d7aa66accf8.jpg',
         'takeout_date' => (\Carbon\Carbon::now())->addDay()
     ];
