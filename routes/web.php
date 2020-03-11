0393780f0ecfe0956d6f01bd137c8ef97c057ed2
jstam89 <jer.stam89@gmail.com>
2020-03-11 19:36:14 +0100

    route fix

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')
