1d444efd0fe5880cafab03b140f0196b75914bb7
jstam89 <jer.stam89@gmail.com>
2020-03-19 22:11:37 +0100

    vue fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.config.publicPath='public_html';
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/assets/js/app.js', 'public/js/main')
     .sass('resources/sass/app.scss', 'public/css');
 
 
