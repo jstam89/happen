8781bbde0fd9669831cf5b484ae3309bb8705f5f
jstam89 <jer.stam89@gmail.com>
2020-04-09 08:58:54 +0200

    vue fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
 <script src="{{ asset('black') }}/js/black-dashboard.min.js?v=1.0.0"></script>
 <script src="{{ asset('black') }}/js/theme.js"></script>
 <script src="{{ mix('js/app.js') }}" defer></script>
-<script src="public/js/app.js"></script>
+<script src="/js/app.js"></script>
 
 
 @stack('js')
